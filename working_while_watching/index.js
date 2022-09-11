3aaeb465e06a473dffb43a8573662a745d951e40
Bikash das <bikash.das@portonics.com>
2022-09-11 14:36:46 +0600

    'admin' path repfix added for admin routes.

--- working_while_watching/index.js
+++ working_while_watching/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 
-const shopRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const adminRoutes = require('./routes/admin');
 
 const bodyParser = require('body-parser');
@@ -10,7 +10,16 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use((req,res,next)=>{
+    console.log(`Gen middleware for path: ${req.url}`);
+    next();
+});
+
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
-app.use(adminRoutes);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found!!</h1>');
+});
 
 app.listen(3000);
--- working_while_watching/routes/admin.js
+++ working_while_watching/routes/admin.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>');
 });
 
 
