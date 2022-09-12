1973bed3c148298c7305b1e40551715810d60e66
Bikash das <bikash.das@portonics.com>
2022-09-12 19:28:31 +0600

    WWT : products saving on local disk added.

--- working_while_watching/controllers/products.js
+++ working_while_watching/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../model/product').Product;
 
 
-const products = Product.fetchAll();
+// const products = Product.fetchAll();
 
 exports.getAddProduct = (req,res,next)=>{
     res.render('add-product',{
@@ -11,16 +11,20 @@ exports.getAddProduct = (req,res,next)=>{
 };
 
 exports.postAddProduct = (req,res,next)=>{
-    console.log('From products.js',products);
-    products.push(new Product(req.body.title));
-    res.redirect('/');
+    // console.log('From products.js',products);
+    // products.push(new Product(req.body.title));
+    new Product(req.body.title).save(()=>{
+        res.redirect('/');
+    });    
 };
 
 exports.getProducts = (req,res,next)=>{
-    console.log('From shop.js',products);
-    res.render('shop',{
-        prods : products,
-        pageTitle: 'Shop',
-        path: '/'
+    Product.fetchAll((prods)=>{
+        console.log('From shop.js',prods);
+        res.render('shop',{
+            prods : prods,
+            pageTitle: 'Shop',
+            path: '/'
+        });
     });
 };
--- working_while_watching/model/product.js
+++ working_while_watching/model/product.js
@@ -1,18 +1,64 @@
+const fs = require('fs');
+const path = require('path');
+
 
 const products = [];
 
-exports.Product = class Product{
+const filePath = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+function doWithProducts(callBack){    
+
+    fs.readFile(filePath,(err,fileContent)=>{
+        let curProducts = [];
+        if(!err){
+            try {
+                const prods = JSON.parse(fileContent);
+                if(Array.isArray(prods) && prods.length > 0){
+                    curProducts = prods;
+                }
+            } catch (error) {
+                console.log(error);
+            }
+            
+        }
+        console.log('curProducts',curProducts);
+        callBack(curProducts);
+    });
+}
+
+function saveProducts(products,callBack){    
+
+    fs.writeFile(filePath,JSON.stringify(products),(err)=>{
+        if(err){
+            console.log(err);
+        }
+        callBack();
+    });
+}
+
+const Product = class {
 
     constructor(t){
         this.title = t;
     }
 
-    save(){
-        products.push(this);
+    save(callBack){
+        doWithProducts((prods)=>{
+            prods.push(this);
+            console.log('during save',prods);
+            saveProducts(prods,callBack);
+        });
+        // products.push(this);
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(callBack){
+        return doWithProducts(callBack);
     }
 
 }
+
+exports.Product = Product;
